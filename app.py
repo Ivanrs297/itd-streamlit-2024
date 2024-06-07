85021d9732ac7e62c2c9bcc0043ac6bb72658c06
Ivan <ivanrs297@gmail.com>
2024-06-06 19:47:55 -0600

    Adding dataframe

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.header("Hola mundo")
 st.title("La clase la de Ivan es la mejor")
 st.markdown("**holaaaa**")
+
+df = pd.read_csv("data.csv")
+
+st.dataframe(df)
